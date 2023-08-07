e189dd874eae48e048fc1e0dbdac1463ee870d68
A4thur <arthurbatista214@gmail.com>
2023-08-07 14:18:45 -0300

    aula

---
+++ backend/resources/views/psdesafio.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PS DESAFIO ADAPTI 2023/2</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
+        .header {
+            background-color: #0074d9;
+            color: white;
+            text-align: center;
+            padding: 20px;
+        }
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+        .link-list {
+            list-style-type: none;
+            padding: 0;
+            text-align: center;
+        }
+        .link-list li {
+            margin-bottom: 10px;
+        }
+        .link-list a {
+            color: #0074d9;
+            text-decoration: none;
+            font-weight: bold;
+            transition: color 0.3s;
+        }
+        .link-list a:hover {
+            color: #ff4136;
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+<div class="header">
+    <h1>PS DESAFIO ADAPTI 2023/2</h1>
+</div>
+<div class="container">
+    <h2 style="display: flex;justify-content: center">Links para Instalação:</h2>
+    <ul class="link-list">
+        <li><a target="_blank "href="https://laravel.com/docs/10.x">Documentação do Laravel</a></li>
+        <li><a target="_blank" href="https://git-scm.com/downloads">Instalação do Git</a></li>
+        <li><a target="_blank" href="https://www.php.net/downloads.php">Instalação do PHP</a></li>
+        <li><a target="_blank " href="https://getcomposer.org/download/">Instalação do Composer</a></li>
+        <li><a target="_blank" href="https://www.apachefriends.org/pt_br/index.html">Instalação do XAMPP</a></li>
+        <li><a target="_blank" href="https://dbeaver.io/download/">Instalação do DBeaver</a></li>
+        <li><a target="_blank" href="https://code.visualstudio.com/">Instalação do VS Code</a></li>
+        <li><a target="_blank" href="https://github.com/">Criar conta no GitHub</a></li>
+    </ul>
+</div>
+</body>
+</html>
